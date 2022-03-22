ab066ee3eeaf56afa69406e0b92a6f3071ee9dc0
DESKTOP-6PA949H\tgikl <torog@stud.ntnu.no>
2022-03-22 17:53:26 +0100

    Added the bare bones of the shopping cart the cart is missing

--- project/js/script.js
+++ project/js/script.js
@@ -2,10 +2,16 @@ window.onload = startUp;
 
 function startUp() {
     document.getElementById("login").onclick = login;
-    document.getElementById("")
+    document.getElementById("login-html").onclick = loginHTML;
+
+
 
 }
 
 function login() {
     window.location.href = "html/login.html";
+}
+
+function loginHTML() {
+    window.location.href = "login.html";
 }
