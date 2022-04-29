180efd774dc097313072d24bfd4c12a33a0bbf36
Anders <amfrost@stud.ntnu.no>
2022-04-29 16:31:08 +0200

    Created more cart functions with items, price, counter etc, with setup for requesting cart list from backend.

--- project/js/cart.js
+++ project/js/cart.js
@@ -1,4 +1,8 @@
 
+//Basic setup for the cart
+
+let cartList = [];
+
 let cartTemplate = document.createElement("template");
 
 cartTemplate.innerHTML = `
@@ -6,16 +10,33 @@ cartTemplate.innerHTML = `
     <div class="cartContainer">
         <div class="cartHeader">
             <h1 class="cartH1">Shopping Cart</h1>
-            <h2 class="removeH2">Remove all</h2>
+            <h2 class="removeH2" onclick="addTestItem()">Add Test Item</h2>
+        </div>
+            <div id="cartItems">
+            </div>
         </div>
     </div>
 </div>
 `
+//Functions for showing and hiding the cart
 
-function append() {
+function openCart() {
   document.getElementById("cartModalDiv").appendChild(cartTemplate.content);
   document.getElementById("cartModalDiv").style.display = "block";
 }
+
+function requestCartList() {
+  let request = new XMLHttpRequest();
+  request.open("GET", "http://localhost:8080/user/cart");
+  request.send();
+
+  request.onload = parseList;
+
+  function parseList() {
+    cartList = request.responseText;
+  }
+}
+
 
 function close() {
   document.getElementById("cartModalDiv").style.display = "none";
@@ -25,4 +46,66 @@ window.onclick = function(event) {
   if (event.target === document.getElementById("cartModalDiv")) {
     document.getElementById("cartModalDiv").style.display = "none";
   }
+}
+
+//Functions for adding items to the cart
+//TODO images not showing correctly
+
+function addItems() {
+
+  for(let i=0; i < cartList.length; i++) {
+
+    let itemTemplate = document.createElement("template")
+
+    itemTemplate.innerHTML = `
+    <div class="item">
+        <div class="cartImages">
+            <img src="../images/borsalino_helmet.png" alt="helmet">
+        </div>
+        <div class="itemInfo">
+            <h3 class="itemTitle">
+                Borsalino Helmet
+            </h3>
+            <h4 class="itemDescription">
+                123 KR     
+            </h4>
+        </div>
+        <div class="itemCounter">
+            <div class="addButton">+</div>
+            <div class="itemCount">1</div>
+            <div class="removeButton">-</div>
+        </div>
+    </div>
+  `
+
+    document.getElementById("cartItems").appendChild(itemTemplate.content)
+
+  }
+}
+
+function addTestItem() {
+  let itemTemplate = document.createElement("template")
+
+  itemTemplate.innerHTML = `
+    <div class="item">
+        <div class="cartImages">
+            <img src="../images/borsalino_helmet.png" alt="helmet" height="200", width="200">
+        </div>
+        <div class="itemInfo">
+            <h3 class="itemTitle">
+                Borsalino Helmet
+            </h3>
+            <h4 class="itemDescription">
+                123 KR     
+            </h4>
+        </div>
+        <div class="itemCounter">
+            <div class="addButton">+</div>
+            <div class="itemCount">1</div>
+            <div class="removeButton">-</div>
+        </div>
+    </div>
+  `
+
+  document.getElementById("cartItems").appendChild(itemTemplate.content)
 }
