ea81e4b095ed7e9c4cd2e3bab3de395c136dc411
Thomas <thomasystenes@gmail.com>
2022-05-27 19:28:20 +0200

    Added optional async parameter to requests.sendApiGetRequest(). Default is true, so unless manualle called with false, should still be synchronious as normal, so should not interfere with previous usage of method.

--- project/js/requests.js
+++ project/js/requests.js
@@ -11,10 +11,14 @@ const API_BASE_URL = "http://localhost:8080/api";
  * @param callback  Callback function to call on success, with response data (JSON-decoded) as the parameter
  * @param errorCallback A function called when the response code is NOT 200. Two parameters will
  * be passed to the function: HTTP response code and response body (as text)
+ * @param async Optional parameter. Set to false if method should be synchronous, I.E, wait
+ * for answer. Else, leave as false;
  */
-function sendApiGetRequest(url, callback, errorCallback){
-    sendApiRequest("get", url, callback, null, errorCallback)
+function sendApiGetRequest(url, callback, errorCallback, async = true){
+    console.log(async)
+    sendApiRequest("get", url, callback, null, errorCallback, async)
 }
+
 
 /**
  * Send HTTP PUT RESP-API request to backend
@@ -58,7 +62,55 @@ function sendApiFileUploadRequest(url, fileContent, callback, errorCallback) {
  * @param callback Callback function to call on success, with response text as the parameter
  * @param requestBody When supplied, send this data in the request body. Does not work with HTTP GET!
  * @param errorCallback A function called when the response code is not 200
+ * @param async Optional parameter. Set to false if method should be synchronous, I.E, wait
+ * for answer. Else, leave as false;
  */
+function sendApiRequest(method, url, callback, requestBody, errorCallback, async = true) {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                let responseJson = "";
+                if (request.responseText) {
+                    responseJson = JSON.parse(request.responseText);
+                }
+                callback(responseJson);
+            } else if (errorCallback) {
+                errorCallback(request.responseText);
+            } else {
+                console.error("Error in API request");
+            }
+        }
+    };
+    request.open(method, API_BASE_URL + url, async);
+    const jwtToken = getCookie("jwt");
+    if (jwtToken) {
+        request.setRequestHeader("Authorization", "Bearer " + jwtToken);
+    }
+    if (requestBody) {
+        if (method.toLowerCase() !== "get") {
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.send(JSON.stringify(requestBody));
+        } else {
+            console.error("Trying to send request data with HTTP GET, not allowed!")
+            request.send();
+        }
+    } else {
+        request.send();
+    }
+}
+
+/**
+ * Send a REST-API request to the backend
+ * @param method The method to use: GET, POST, PUT, DELETE
+ * @param url relative URL of the API endpoint
+ * @param callback Callback function to call on success, with response text as the parameter
+ * @param requestBody When supplied, send this data in the request body. Does not work with HTTP GET!
+ * @param errorCallback A function called when the response code is not 200
+ * @param async Optional parameter. Set to false if method should be synchronous, I.E, wait
+ * for answer. Else, leave as false;
+ */
+/*
 function sendApiRequest(method, url, callback, requestBody, errorCallback) {
     const request = new XMLHttpRequest();
     request.onreadystatechange = function () {
@@ -93,3 +145,4 @@ function sendApiRequest(method, url, callback, requestBody, errorCallback) {
         request.send();
     }
 }
+*/
