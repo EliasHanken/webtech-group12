115dc66228e83fb17012a0958dc8e43c5a89d85d
Thomas <thomasystenes@gmail.com>
2022-05-30 22:26:18 +0200

    Continued work on fecthing images from backend. Images are downloaded as byte[], but have not been able to display them yet.,

--- project/js/requests.js
+++ project/js/requests.js
@@ -18,6 +18,19 @@ function sendApiGetRequest(url, callback, errorCallback, async = true){
     sendApiRequest("get", url, callback, null, errorCallback, async)
 }
 
+/**
+ * Semd HTTP GET REST-API request to backend (SHOULD BE USED WHEN USING GET TO FETCH IMAGES)
+ * @param url relative URL of the API endpoint
+ * @param callback  Callback function to call on success, with response data (JSON-decoded) as the parameter
+ * @param errorCallback A function called when the response code is NOT 200. Two parameters will
+ * be passed to the function: HTTP response code and response body (as text)
+ * @param async Optional parameter. Set to false if method should be synchronous, I.E, wait
+ * for answer. Else, leave as false;
+ */
+function sendApiGetImageRequest(url, callback, errorCallback, async = true){
+    sendApiImageRequest("get", url, callback, null, errorCallback, async)
+}
+
 
 /**
  * Send HTTP PUT RESP-API request to backend
@@ -99,27 +112,22 @@ function sendApiRequest(method, url, callback, requestBody, errorCallback, async
     }
 }
 
-/**
- * Send a REST-API request to the backend
- * @param method The method to use: GET, POST, PUT, DELETE
- * @param url relative URL of the API endpoint
- * @param callback Callback function to call on success, with response text as the parameter
- * @param requestBody When supplied, send this data in the request body. Does not work with HTTP GET!
- * @param errorCallback A function called when the response code is not 200
- * @param async Optional parameter. Set to false if method should be synchronous, I.E, wait
- * for answer. Else, leave as false;
- */
-/*
-function sendApiRequest(method, url, callback, requestBody, errorCallback) {
+function sendApiImageRequest(method, url, callback, requestBody, errorCallback, async = true){
+    console.log("Calling sendApiGetImageRequest()")
+    let image = document.createElement("img");
     const request = new XMLHttpRequest();
     request.onreadystatechange = function () {
+        //Check content type.
+        contentType = this.getResponseHeader("content-type")
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
                 let responseJson = "";
                 if (request.responseText) {
-                    responseJson = JSON.parse(request.responseText);
+                    image.src = request.responseText;
+                    //console.log("Type = " + typeof image)
+                    console.log("image.src = " + image.src)
                 }
-                callback(responseJson);
+                callback(image);
             } else if (errorCallback) {
                 errorCallback(request.responseText);
             } else {
@@ -127,21 +135,21 @@ function sendApiRequest(method, url, callback, requestBody, errorCallback) {
             }
         }
     };
-    request.open(method, API_BASE_URL + url);
-    const jwtToken = getCookie("jwt");
-    if (jwtToken) {
-        request.setRequestHeader("Authorization", "Bearer " + jwtToken);
+    request.open(method, API_BASE_URL + url, async);
+    const jwtToken = getCookie("jwt")
+    if(jwtToken) {
+        request.setRequestHeader('Content-Type', 'application/json') //Should this be image?
     }
-    if (requestBody) {
-        if (method.toLowerCase() !== "get") {
-            request.setRequestHeader('Content-Type', 'application/json');
+    if(requestBody) {
+        if(method.toLowerCase()!== get) {
+            request.setRequestHeader('Content-Type', 'application/json')
             request.send(JSON.stringify(requestBody));
         } else {
             console.error("Trying to send request data with HTTP GET, not allowed!")
             request.send();
         }
     } else {
-        request.send();
+        request.send()
     }
+    return image
 }
-*/
