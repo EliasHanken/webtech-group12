7a942a3e96fb4262a8a62747c05ca5d476476dc1
Thomas <thomasystenes@gmail.com>
2022-06-06 11:58:09 +0200

    Image download from backend now works for bikes. Next to make same adjustments to other products.

--- project/js/ProductCardGenerator.js
+++ project/js/ProductCardGenerator.js
@@ -3,9 +3,11 @@
 Class for generating and add product cards to HTML.
  */
 
-/* List containing products for generating product cards */
+/* Lists containing bikes/products/images for generating product cards */
+let PRODUCT_CARD_BIKES = [];
+let PRODUCT_CARD_BIKE_IMAGES = [];
 let PRODUCT_CARD_PRODUCTS = [];
-let PRODUCT_CARD_IMAGES = [];
+let PRODUCT_CARD_PRODUCT_IMAGES = [];
 
 /**
  * Generates product card skeleton, with ids for easy access to elements
@@ -14,18 +16,18 @@ function generateProductCardSkeleton(){
     document.getElementById("owl-carousel-index-page").innerHTML =
         //FIRST CARD
         '<article class="card" id="carousel-card-1">' +
-        '<div class="card-img-div" id="card-img-1">' +
-        '<!-- Img tag goes here -->' +
+        '<div class="card-img-div">' +
+        '<img src="" alt="A red bike of our Commuter model" id="card-img-1" height="150px" width="200px">' +
         '</div>' +
         '<div class="card-name" id="card-name-1">' +
-        '<P>' + PRODUCT_CARD_PRODUCTS[0].bikeModelName + '</P>' +
+        '<P>' + PRODUCT_CARD_BIKES[0].bikeModelName + '</P>' +
         '</div>' +
         '<div class="card__precis">' +
         '   <a href="" class="card-icon">' +
         '       <ion-icon name="heart-outline"></ion-icon>' +
         '   </a>' +
         '   <div>' +
-        '       <span class="card-price" id="card-price-1">' + PRODUCT_CARD_PRODUCTS[0].price + ' /min</span>' +
+        '       <span class="card-price" id="card-price-1">' + PRODUCT_CARD_BIKES[0].price + ' /min</span>' +
         '   </div>' +
         '   <a href="" class="card-icon">' +
         '       <ion-icon name="cart-outline"></ion-icon>' +
@@ -37,14 +39,14 @@ function generateProductCardSkeleton(){
         '<!-- Img tag goes here -->' +
         '</div>' +
         '<div class="card-name" id="card-name-2">' +
-        '<p>' + PRODUCT_CARD_PRODUCTS[1].name + '</p>' +
+        '<p>' + PRODUCT_CARD_PRODUCTS[0].name + '</p>' +
         '</div>' +
         '<div class="card__precis">' +
         '   <a href="" class="card-icon">' +
         '       <ion-icon name="heart-outline"></ion-icon>' +
         '   </a>' +
         '   <div>' +
-        '       <span class="card-price" id="card-price-2">' + PRODUCT_CARD_PRODUCTS[1].price + '</span>' +
+        '       <span class="card-price" id="card-price-2">' + PRODUCT_CARD_PRODUCTS[0].price + '</span>' +
         '   </div>' +
         '   <a href="" class="card-icon">' +
         '       <ion-icon name="cart-outline"></ion-icon>' +
@@ -56,14 +58,14 @@ function generateProductCardSkeleton(){
         '<!-- Img tag goes here -->' +
         '</div>' +
         '<div class="card-name" id="card-name-3">' +
-        '<p>' + PRODUCT_CARD_PRODUCTS[2].name +'</p>' +
+        '<p>' + PRODUCT_CARD_PRODUCTS[1].name +'</p>' +
         '</div>' +
         '<div class="card__precis">' +
         '   <a href="" class="card-icon">' +
         '       <ion-icon name="heart-outline"></ion-icon>' +
         '   </a>' +
         '   <div>' +
-        '       <span class="card-price" id="card-price-3">' + PRODUCT_CARD_PRODUCTS[2].price + '</span>' +
+        '       <span class="card-price" id="card-price-3">' + PRODUCT_CARD_PRODUCTS[1].price + '</span>' +
         '   </div>' +
         '   <a href="" class="card-icon">' +
         '       <ion-icon name="cart-outline"></ion-icon>' +
@@ -75,14 +77,14 @@ function generateProductCardSkeleton(){
         '<!-- Img tag goes here -->' +
         '</div>' +
         '<div class="card-name" id="card-name-4">' +
-        '<p>' + PRODUCT_CARD_PRODUCTS[3].name + '</p>' +
+        '<p>' + PRODUCT_CARD_PRODUCTS[2].name + '</p>' +
         '</div>' +
         '<div class="card__precis">' +
         '   <a href="" class="card-icon">' +
         '       <ion-icon name="heart-outline"></ion-icon>' +
         '   </a>' +
         '   <div>' +
-        '       <span class="card-price" id="card-price-4">' + PRODUCT_CARD_PRODUCTS[3].price + '</span>' +
+        '       <span class="card-price" id="card-price-4">' + PRODUCT_CARD_PRODUCTS[2].price + '</span>' +
         '   </div>' +
         '   <a href="" class="card-icon">' +
         '       <ion-icon name="cart-outline"></ion-icon>' +
@@ -100,11 +102,17 @@ function generateProductCardSkeleton(){
  * @param imgTag4 img tag for the fourth card
  */
 function generateProductCardImgTags(){
-    //document.getElementById("card-img-1").src = "fasd";
+
+    console.log("generateProductCardImgTags() -> " + PRODUCT_CARD_BIKES[0].imgData)
+    document.getElementById("card-img-1").src =
+        "data:image/png;base64," + PRODUCT_CARD_BIKES[0].imgData;
+
+
+    console.log(document.getElementById("card-img-1").innerHTML)
     //document.getElementById("card-img-1").innerHTML =
     //    '<img src="images/textile-bag-cropped.jpg" alt="Image of a canvas bag">'
     document.getElementById("card-img-1").innerHTML =
-        '<img src="' + PRODUCT_CARD_IMAGES[0] + '" alt="Image of a canvas bag">'
+        '<img src="' + PRODUCT_CARD_PRODUCT_IMAGES[0] + '" alt="Image of a canvas bag">'
         document.getElementById("card-img-2").innerHTML =
             '<img src="images/textile-bag-cropped.jpg" alt="Image of a canvas bag">'
     document.getElementById("card-img-3").innerHTML =
@@ -115,7 +123,7 @@ function generateProductCardImgTags(){
     /*
     const imgTag = document.getElementById("card-img-1")
     console.log("src before: " + imgTag.src);
-    //document.getElementById("card-img-1").src = PRODUCT_CARD_IMAGES[0].src;
+    //document.getElementById("card-img-1").src = PRODUCT_CARD_PRODUCT_IMAGES[0].src;
     console.log("src after: " + document.getElementById("card-img-1").src)
      */
 }
@@ -153,7 +161,6 @@ function generateProductCardsTotal(products){
     generateProductCardSkeleton();
     //generateProductCardSkeleton(products);
 
-
     generateProductCardImgTags(
         //'<img src="images/bike.jpg" alt="Image of a bike">',
     //'<img src="https://i5.walmartimages.com/asr/3f3091dd-b7fe-4182-8596-87fc0aa7f83f.ef8ba555d0f1375798000d14d92bc733.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="Picture of bike">',
@@ -169,35 +176,43 @@ function generateProductCardsTotal(products){
  * to the PRODUCT_CARD_PRODUCTS array.
  */
 function getProductDataForCards(){
-    console.log("Requesting product data for cards...");
-    sendApiGetRequest("/bikes/8", assignToProductCardProducts, productCallbackOnFail, false);
-    sendApiGetRequest("/products", assignToProductCardProducts, productCallbackOnFail, false);
-    console.log("Request done - > " + PRODUCT_CARD_PRODUCTS)
+    sendApiGetRequest("/bikes", assignBikesToProductCardProducts, productCallbackOnFail, false);
+    sendApiGetRequest("/products", assignProductsToProductCardProducts, productCallbackOnFail, false);
+    //--- CURRENTLY NOT BEING USED, AS IMAGES ARE NOW STORED IN THE BIKE OBJECT IN BACK END --->
     //sendApiGetRequest("/images/" + PRODUCT_CARD_PRODUCTS[0].imgId,
     //    populateImageTag, productCallbackOnFail, false);
-    let image  = null;
-    image = sendApiGetImageRequest("/images/" + PRODUCT_CARD_PRODUCTS[0].imgId,
-        assignToProductCardImages, productCallbackOnFail, false);
+    //let image  = null;
+    //image = sendApiGetImageRequest("/images/" + PRODUCT_CARD_BIKES[0].bikeModelName.toLowerCase(),
+    //    assignToProductCardImages, productCallbackOnFail, false);
+
+    console.log("End of getProductDataForCards()");
 }
 
 /**
  * Assigns the supplied products to the PRODUCT_CARD_PRODUCTS array.
+ * Should NOT be used for adding bikes; use assignBikesToProductCardProducts() for that.
  * @param products Products to be assigned to list.
  */
-function assignToProductCardProducts(products){
-    //If products is an array:
-    if(Array.isArray(products)) {
-        for (let i = 0; i < products.length; i++) {
-            //console.log(products[i]);
-            PRODUCT_CARD_PRODUCTS.push(products[i]);
-        }
-    } else { //Of products is NOT an array, I.E, single item
-        PRODUCT_CARD_PRODUCTS.push(products)
+function assignProductsToProductCardProducts(products){
+    for(i = 0; i < products.length; i++){
+        PRODUCT_CARD_PRODUCTS.push(products[i])
     }
 }
 
 /**
- * Assigns the supplied image(s) to the PRODUCT_CARD_IMAGES array;
+ * Assigns bikes in bikes to the PRODUCT_CARD_PRODUCTS array.
+ * @param bikes Array of Bikes, the objects of which will be added to
+ * PRODUCT_CARD_PRODUCTS.
+ */
+function assignBikesToProductCardProducts(bikes){
+    for(i = 0; i < bikes.length; i++){
+        PRODUCT_CARD_BIKES.push(bikes[i]);
+    }
+}
+
+
+/**
+ * Assigns the supplied image(s) to the PRODUCT_CARD_PRODUCT_IMAGES array;
  * @param image Images to be assigned to the list.
  */
 function assignToProductCardImages(images){
@@ -207,12 +222,14 @@ function assignToProductCardImages(images){
     //If images is an array
     if(Array.isArray(images)) {
         for (let i = 0; i < images.length; i++) {
-            PRODUCT_CARD_IMAGES.push((images[i]))
+            PRODUCT_CARD_PRODUCT_IMAGES.push((images[i]))
         }
     } else {    //If images is NOT an array, I.E, single item
-        PRODUCT_CARD_IMAGES.push(images);
+        PRODUCT_CARD_PRODUCT_IMAGES.push(images);
     }
 }
+
+
 
 function productCallbackOnFail(){
     console.log("An error occurred on sendApiGetRequest.")
